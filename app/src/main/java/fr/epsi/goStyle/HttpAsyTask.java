56b8a02a2caabd70e5d5562475b234ee24510396
Alexis <alexis.heroin@epsi.fr>
2020-02-05 13:13:32 +0100

    remove token

--- app/src/main/java/fr/epsi/goStyle/HttpAsyTask.java
+++ app/src/main/java/fr/epsi/goStyle/HttpAsyTask.java
@@ -69,7 +69,6 @@ public class HttpAsyTask extends AsyncTask<Void,Void,Object> {
         URL url = new URL(urlStr);
         HttpURLConnection client = null;
 
-        token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImN0eSI6IkpXVCJ9.eyJpYXQiOjE1ODA5MDM1OTcsImV4cCI6MTU4MDkwNzE5NywiZW1haWwiOiJhbGV4aXMuaGVyb2luQGVwc2kuZnIiLCJpZCI6MjR9.UTcK93Ac1RhmbDd4VL8d1pFUg6Q_I-Cy1WWJTyWMAyc--XhDx0B84-aAy7JtELTH4OLNEmuEBF-33mPERlK72cDqs_wx4C28l0NPP-U35Hrx-tbu1nxMJuk80Mm1QwOrVDZ58ECHcr0GZFy15ie1-fwEzqC1PUXex6g8mqo5wHIzqORP9S4w5ojtvumVR8rIJxZ5B03ww-gryttg0I9YToc-KblCIfPdfH_Btc5ytek-whRJsUpYP0wocKfExLEF8vt3bezTHoXtsYaZQ792zcg_7OiCR45EVueFHN_WcRAofyljCg1k_kCEzz3nvesS_K5RNXQt6BfRtFJBUW5r_WPXP13BViRpcTsRMBf42t3kQUMufJdEr1RMds0LFNABvvLjYvgDc8kx6t_WuSc4Vz-CJcPe5O7Jpz0WYG2D4gj-6IBfvIAbzPYa5rAuptR-FMUQbeB-zaoHAqHlVrM41JqTh1BRTQJ41YBRHKB4d6zFMvROVM0J1YlNP9mM6Ad_s6V7AEFrNAuG7VWmogRnw-0Sys5KrGCmvJk_aiD3epCkQTRFnXXtmO-K-ztkJaJl_y7Cps_Q4YAeJL27IJf3JvNjiLkLUoF1czm5W8riChwp2b4Z4cW-FxXbpwPqRIKc8c5mm5QatxYoRArK7RCuOEoNa4yYYF1P-nhVlZhqkUw";
         try {
             client = (HttpURLConnection) url.openConnection();
             client.setRequestMethod(method);
